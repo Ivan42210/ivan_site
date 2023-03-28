206e538f066954b9ac67908063782142c9d80416
Ivan42210 <ivan.cha.42@outlook.fr>
2023-03-29 00:40:36 +0200

    modifications

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ import CardSection from "../components/CardSection";
 
 function Home({datas}) {
 
-const BannerTitle = 'TRADUCTION - WEBDESIGN - FORMATION'
+const BannerTitle = 'TRADUCTION - WEB DESIGN - FORMATION'
 const textBanner = 'Un service humain avant tout'
 
   return (
